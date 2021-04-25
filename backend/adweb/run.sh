cfe519e51569e003cceeee15def5b72f93c21865
Yan Hua <yhsilver@outlook.com>
2021-04-26 02:38:12 +0800

    modify run.sh

--- backend/adweb/run.sh
+++ backend/adweb/run.sh
@@ -1,4 +1,7 @@
-docker run -p 9421:9421 -t adweb/eureka-discovery &
-docker run -p 9423:9423 -t adweb/gateway &
-docker run -p 9422:9422 -t adweb/welcome &
-docker run -p 9425:9425 -t adweb/user-service &
+docker run -p 9421:9421 --net=host -t   adweb/eureka-discovery &
+sleep 30
+docker run -p 9423:9423 --net=host -t adweb/gateway &
+sleep 30
+docker run -p 9422:9422 --net=host -t adweb/welcome &
+sleep 30
+docker run -p 9425:9425 --net=host -t adweb/user-service &
