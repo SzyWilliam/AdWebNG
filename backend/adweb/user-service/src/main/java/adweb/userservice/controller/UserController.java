fd1336dd3cb87be9f6aedf5f0e2212f79500c7d3
Yan Hua <yhsilver@outlook.com>
2021-05-10 10:46:03 +0800

    Refactor UserService and add token services

--- backend/adweb/user-service/src/main/java/adweb/userservice/controller/UserController.java
+++ backend/adweb/user-service/src/main/java/adweb/userservice/controller/UserController.java
@@ -1,12 +1,14 @@
 package adweb.userservice.controller;
+
 import adweb.userservice.controller.requests.LoginRequest;
 import adweb.userservice.controller.requests.RegisterRequest;
 import adweb.userservice.exception.EmailExistsException;
 import adweb.userservice.exception.EmailNotRegisteredException;
 import adweb.userservice.exception.InternalServerError;
 import adweb.userservice.exception.WrongPasswordException;
-import adweb.userservice.service.UserLoginService;
-import adweb.userservice.service.UserRegisterService;
+import adweb.userservice.security.token.TokenUtil;
+import adweb.userservice.security.token.VerifyToken;
+import adweb.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,22 +18,40 @@ import java.util.HashMap;
 @RequestMapping(path = "/auth")
 public class UserController {
 
-    private UserLoginService userLoginService;
-    private UserRegisterService userRegisterService;
+    private UserService userService;
+
 
     @Autowired
-    public UserController(UserLoginService userLoginService, UserRegisterService userRegisterService) {
-        this.userLoginService = userLoginService;
-        this.userRegisterService = userRegisterService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+
     }
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public @ResponseBody HashMap<String, Object> userLogin(@RequestBody LoginRequest loginRequest) throws EmailNotRegisteredException, WrongPasswordException {
-        return userLoginService.login(loginRequest);
+    @ResponseBody
+    public HashMap<String, Object> userLogin(@RequestBody LoginRequest loginRequest) throws EmailNotRegisteredException, WrongPasswordException {
+        return userService.login(loginRequest.getEmail(), loginRequest.getPassword());
     }
 
     @RequestMapping(path = "/register", method = RequestMethod.POST)
-    public @ResponseBody HashMap<String, Object> useRegister(@RequestBody RegisterRequest registerRequest) throws InternalServerError, EmailExistsException {
-        return userRegisterService.register(registerRequest);
+    @ResponseBody
+    public HashMap<String, Object> useRegister(@RequestBody RegisterRequest registerRequest) throws InternalServerError, EmailExistsException {
+        return userService.register(registerRequest.getEmail(), registerRequest.getPassword(),
+                registerRequest.getFullName());
     }
+
+
+    /**
+     * token test
+     */
+    @VerifyToken
+    @GetMapping("/username")
+    @ResponseBody
+    public String getLoggedUsername(@RequestHeader("token") String token) {
+        System.out.println(token);
+        String username = userService.getUserByEmail(TokenUtil.getEmailFromToken(token)).getUsername();
+        System.out.println(username);
+        return username;
+    }
+
 }
---
+++ backend/adweb/user-service/src/main/java/adweb/userservice/exception/UserNotExistException.java
@@ -0,0 +1,3 @@
+package adweb.userservice.exception;
+
+public class UserNotExistException extends Exception{}
