58bca1e9e7c34ff2f74a6c9ded98ad948da1322e
songziyang.william <songziyang.william@bytedance.com>
2021-06-16 11:47:53 +0800

    add dockerfile

--- backend/adweb/eureka-discovery/src/main/java/adweb/eurekadiscovery/EurekaDiscoveryApplication.java
+++ backend/adweb/eureka-discovery/src/main/java/adweb/eurekadiscovery/EurekaDiscoveryApplication.java
@@ -1,4 +1,4 @@
-﻿package adweb.eurekadiscovery;
+package adweb.eurekadiscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
